1a0029b1fcc110b4929ef13a332ca748c358968a
Steven Tharwat <stevven.helmy@gmail.com>
2025-11-22 14:18:51 +0200

    Time: 72 ms (15.49%), Space: 39.9 MB (37.68%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cs
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cs
@@ -1,5 +1,22 @@
 public class Solution {
     public string DefangIPaddr(string address) {
-        return address.Replace(".", "[.]");
+        char[] chars = new char[address.Length + 3 * 2];
+int charsIndex = 0;
+for (int i = 0 ; i < address.Length ; i++, charsIndex++)
+{
+    if (address[i] == '.')
+    {
+        chars[charsIndex] = '[';
+        chars[charsIndex + 1] = '.';
+        chars[charsIndex + 2] = ']';
+        charsIndex += 2;
+    }
+    else
+    {
+        chars[charsIndex] = address[i];
+    }
+
+}
+return new string(chars);
     }
 }
