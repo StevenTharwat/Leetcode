c5c2d6bc4a1a4b0b46fdf6fc22dd96d17d3cc218
Steven Tharwat <stevven.helmy@gmail.com>
2026-01-20 12:02:00 +0200

    Time: 14 ms (34.9%), Space: 52.7 MB (48.59%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public string Convert(string s, int numRows) {
+        if (s.Length <= 2 || numRows < 2 || s.Length <= numRows)
+    return s;
+var arrayLength = Math.Min(numRows, s.Length);      
+StringBuilder[] rows = new StringBuilder[arrayLength];
+var currentRow = -1;
+var goingDown = true;
+foreach (char c in s)
+{
+    if(goingDown)
+        currentRow++;
+    else
+        currentRow--;
+
+    rows[currentRow] ??= new StringBuilder();
+    rows[currentRow].Append(c);
+
+    if (currentRow == arrayLength-1)
+        goingDown = false;
+    else if (currentRow == 0)
+        goingDown = true;
+}
+return string.Join("", rows.Select(r => r.ToString()));
+    }
+}
